6adbf13231048685c305d3932d12dbfe14b95c25
Matiyas Seifu <88274941+mati1yas@users.noreply.github.com>
2022-01-01 09:27:01 +0300

    Update contest-1.py

--- contest-1.py
+++ contest-1.py
@@ -57,16 +57,16 @@ for i in range(n):
 n=3
 print("DIAMOND")
 for i in range(n):
-    for j in range(n+1-i):
+    for j in range(n-1-i):
         print(" ",end="")
     for k in range(1,i+1):
         print(k,end="")
     for l in range(i+1,0,-1):
         print(l,end="")
     print()
-i=1
-for i in range(n):
-    for j in range(i+2):
+
+for i in range(1,n):
+    for j in range(i):
         print(" ",end="")
     for k in range(1,n-i,1):
         print(k,end="")
