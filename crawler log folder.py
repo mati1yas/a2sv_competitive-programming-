ef9a246e9edbc283de539ed411e2f06e131cc5ad
Matiyas Seifu <88274941+mati1yas@users.noreply.github.com>
2022-01-19 22:27:10 +0300

    Create crawler log folder.py

---
+++ crawler log folder.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        count=0
+        for i in logs:
+            if i=='../':
+                
+                if count==0:
+                    continue
+                else:
+                    count-=1
+            elif i=='./':
+                continue
+            else:
+                count+=1
+        return count
