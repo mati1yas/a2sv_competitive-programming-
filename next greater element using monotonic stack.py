9fee4e2086379fd7c113653e6d863a8ef2221fca
Matiyas Seifu <88274941+mati1yas@users.noreply.github.com>
2022-01-25 14:46:07 +0300

    Create next greater element using monotonic stack.py

---
+++ next greater element using monotonic stack.py
@@ -0,0 +1,19 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        
+        stack=[]
+        dicti={}
+        output=[]
+        for i in reversed(range(len(nums2))):
+           
+            whi  le stack and stack[-1]<nums2[i]:
+                stack.pop()
+            if len(stack)==0:
+                dicti[nums2[i]]=-1
+            else:
+                dicti[nums2[i]]=stack[-1]
+            stack.append(nums2[i])
+        
+        
+        
+        return [dicti[i] for i in nums1 if i in dicti]
