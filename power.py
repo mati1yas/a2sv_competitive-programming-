4017d75f720edba224e7f7b9da5901fa8b9cee39
Matiyas Seifu <88274941+mati1yas@users.noreply.github.com>
2022-01-31 13:06:59 +0300

    Create power.py

---
+++ power.py
@@ -0,0 +1,14 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n<0:
+            n=-1*n
+            x=1/x
+        if n==0:
+            return 1
+        if n%2==0:
+            res=self.myPow(x,n//2)
+            return res*res
+        
+        else:
+            return x*self.myPow(x,n-1)
+        
