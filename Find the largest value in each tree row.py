9fc50e0a04d116d1d9e9d6bfd3a8ea5dc3009caa
Matiyas Seifu <88274941+mati1yas@users.noreply.github.com>
2022-03-24 11:41:22 +0300

    Create Find the largest value in each tree row.py

---
+++ Find the largest value in each tree row.py
@@ -0,0 +1,24 @@
+class Solution:
+    def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
+        res  = []
+        
+        q=collections.deque()
+        q.append(root)
+        
+        
+        while q:
+            qlen=len(q)
+            level = []
+            
+            for i in range(qlen):
+                node=q.popleft()
+            
+                if node:
+                    level.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+            if level:
+                res.append(max(level))
+                
+        return res
+                
